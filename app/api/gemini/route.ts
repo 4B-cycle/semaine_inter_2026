b94480c4047063af6ce4097de6d38a11cc65d1ea
4B-Cycle <b.eloin@students.ephec.be>
2026-04-02 14:08:27 +0200

    autre modele ia

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -32,7 +32,7 @@ export async function POST(request: Request) {
     `;
 
     const response = await ai.models.generateContent({
-      model: "gemini-1.5-flash-8b",
+      model: "gemini-2.5-flash-lite",
       contents: text,
       config: {
         systemInstruction: systemInstruction,
